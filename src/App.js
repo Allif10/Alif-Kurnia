383a5897f1b8284da7f5dcdfedc509ee1fdea06b
Allif10 <162294602+Allif10@users.noreply.github.com>
2024-05-22 23:24:29 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hi, my name is - Allif!</h1>
+        <p>Right now, I am 19 years old and I live in Jatinangor City.</p>
+        <p>My full name is Allif Kurnia and I am a College Student at Padjadjaran University.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
